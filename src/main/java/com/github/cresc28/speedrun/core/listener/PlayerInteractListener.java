e84204cdb5eefa80681a31b4510d61740842abe6
cresc28 <crescendo010828@gmail.com>
2025-07-10 20:07:38 +0900

    docの追加や細かな表記ミスの修正

--- src/main/java/com/github/cresc28/speedrun/core/listener/PlayerInteractListener.java
+++ src/main/java/com/github/cresc28/speedrun/core/listener/PlayerInteractListener.java
@@ -36,6 +36,11 @@ public class PlayerInteractListener implements Listener {
         this.cpManager = cpManager;
     }
 
+    /**
+     * プレイヤーが何かをクリックしたときの処理。
+     *
+     * @param e PlayerInteractEvent
+     */
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent e){
         if (e.getHand() != EquipmentSlot.HAND) return;
@@ -44,40 +49,64 @@ public class PlayerInteractListener implements Listener {
 
         //看板クリックよりこちらの処理を優先する。
         if(item != null && item.getType() == Material.NETHER_STAR) {
-            netherStarClickEvent(e);
+            if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
+                handNetherStarRightClick(e);
+            }
+
+            else if(e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK){
+                handNetherStarLeftClick(e);
+            }
+
             return; //CPでの移動と同時に看板をクリックできないようにする。(不本意なCPセットを防止)
         }
 
         if (clickedBlock != null && clickedBlock.getState() instanceof Sign && e.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            signClickEvent(e, (Sign) clickedBlock.getState());
+            signClick(e, (Sign) clickedBlock.getState());
         }
     }
 
-    private void netherStarClickEvent(PlayerInteractEvent e){
+    /**
+     * プレイヤーがネザークリックをもって右クリックしたときの処理。
+     *
+     * @param e PlayerInteractEvent
+     */
+    private void handNetherStarRightClick(PlayerInteractEvent e){
+        Player player = e.getPlayer();
+        UUID uuid = player.getUniqueId();
+        Location loc;
+
+        if(ConfigManager.isCrossWorldTpAllowed()) loc = cpManager.getGlobalRecentCpLocation(uuid);
+        else loc = cpManager.getLocalRecentCpLocation(uuid);
+
+        if (loc != null) {
+            player.teleport(loc);
+        }
+        else {
+            player.sendMessage(ChatColor.RED + "有効なチェックポイントが存在しません。");
+        }
+    }
+
+    /**
+     * プレイヤーがネザークリックをもって左クリックしたときの処理。
+     *
+     * @param e PlayerInteractEvent
+     */
+    private void handNetherStarLeftClick(PlayerInteractEvent e){
         Player player = e.getPlayer();
         UUID uuid = player.getUniqueId();
 
-        if(e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK){
-            Location loc;
-            if(ConfigManager.isCrossWorldTpAllowed()) loc = cpManager.getGlobalRecentCpLocation(uuid);
-            else loc = cpManager.getLocalRecentCpLocation(uuid);
-
-            if (loc != null) {
-                player.teleport(loc);
-            }
-            else {
-                player.sendMessage(ChatColor.RED + "有効なチェックポイントが存在しません。");
-            }
-        }
-
-        else if(e.getAction() == Action.LEFT_CLICK_AIR || e.getAction() == Action.LEFT_CLICK_BLOCK){
-            e.setCancelled(true); // ブロック破壊を起こさない
-            player.addScoreboardTag("MenuOpen");
-            new CheckpointMenu(player, cpManager, 0, player.getWorld(),false).openInventory();
-        }
+        e.setCancelled(true); // ブロック破壊を起こさない
+        player.addScoreboardTag("MenuOpen");
+        new CheckpointMenu(player, cpManager, 0, player.getWorld(),false).openInventory();
     }
 
-    private void signClickEvent(PlayerInteractEvent e, Sign sign){
+    /**
+     * 設置されている看板をクリックしたときの処理
+     *
+     * @param e PlayerInteractEvent
+     * @param sign 看板
+     */
+    private void signClick(PlayerInteractEvent e, Sign sign){
         Player player = e.getPlayer();
         UUID uuid = player.getUniqueId();
         String firstLine = ChatColor.stripColor(sign.getLine(0));
